cca49753566973125f68c309780e7a54ec04c336
Cristina Petrache <cristinapetrache99@gmail.com>
2021-02-28 16:57:37 +0200

    created package

---
+++ SecondTest/src/ro/ase/csie/HelloTest.java
@@ -0,0 +1,11 @@
+package ro.ase.csie;
+
+public class HelloTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am the second test");
+
+	}
+
+}
