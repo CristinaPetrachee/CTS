847492c3dc58c28b4800b646fd41263146684588
Cristina Petrache <cristinapetrache99@gmail.com>
2021-02-28 17:06:08 +0200

    class Persoane added

--- SecondTest/src/ro/ase/csie/HelloTest.java
+++ SecondTest/src/ro/ase/csie/HelloTest.java
@@ -5,7 +5,10 @@ public class HelloTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I am the second test");
-
+		
+		Persoana p1 = new Persoana("Cristina", "Petrache");
+		
+		System.out.println(p1.toString());
 	}
 
 }
---
+++ SecondTest/src/ro/ase/csie/Persoana.java
@@ -0,0 +1,32 @@
+package ro.ase.csie;
+
+public class Persoana {
+	
+	private String firstName;
+	private String lastName;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	@Override
+	public String toString() {
+		return "Persoana [firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+	
+	public Persoana(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	
+}
