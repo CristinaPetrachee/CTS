956863e76cf9cf31237b8cc0e41ebcafdafbd900
Cristina Petrache <cristinapetrache99@gmail.com>
2021-03-04 14:16:12 +0200

    tested first weekDay function

--- Seminar2/src/ro/ase/csie/cts/seminar2/Main.java
+++ Seminar2/src/ro/ase/csie/cts/seminar2/Main.java
@@ -3,8 +3,8 @@ package ro.ase.csie.cts.seminar2;
 public class Main {
 
 	public static void main(String[] args) {
-		//To do - test the calendar function
-
+		CalendarUtil cu = new CalendarUtil();
+		System.out.println(cu.weekDay(1));
 	}
 
 }
