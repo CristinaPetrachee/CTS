4b9587f56458f81a0b027c0e9e6066b1109d6d91
Cristina Petrache <cristinapetrache99@gmail.com>
2021-03-18 14:02:04 +0200

    added Sem4 - static factory method

---
+++ Seminar1087/src/ro/ase/csie/cts/sem4/Main.java
@@ -0,0 +1,36 @@
+package ro.ase.csie.cts.sem4;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+import ro.ase.csie.cts.sem4.banking.BankAccount;
+import ro.ase.csie.cts.sem4.banking.CreditBankAccount;
+import ro.ase.csie.cts.sem4.banking.DebitBankAccount;
+import ro.ase.csie.cts.sem4.banking.EmailNotificationService;
+import ro.ase.csie.cts.sem4.banking.NotificationService;
+import ro.ase.csie.cts.sem4.banking.Person;
+
+public class Main {
+
+	public static void main(String[] args) {
+		NotificationService emailService = new EmailNotificationService();
+
+		Person p3 = new Person("Van Damme");
+		p3.setEmail("van@damme.com");
+		p3.setMobile("+4765223087");
+		
+		//static factory method
+		CreditBankAccount b = CreditBankAccount.createCreditBankAccountWithBalance(emailService, "INGB2345245", p3, 50);
+		
+		CreditBankAccount b1 = CreditBankAccount.createCreditBankAccount(emailService, "BRD2345245", p3);
+
+		BigInteger bi = new BigInteger(24, 11, new Random());
+		BigInteger bi2 = BigInteger.probablePrime(12, new Random());
+		
+		Boolean value = Boolean.valueOf(false);
+		
+		b.deposit(50);
+		System.out.println(b.toString());
+	}
+
+}
---
+++ Seminar1087/src/ro/ase/csie/cts/sem4/banking/Transferable.java
@@ -0,0 +1,6 @@
+package ro.ase.csie.cts.sem4.banking;
+
+public interface Transferable {
+	
+	public void transfer(Receivable destination, long amount) throws InsufficientFundsException;
+}
