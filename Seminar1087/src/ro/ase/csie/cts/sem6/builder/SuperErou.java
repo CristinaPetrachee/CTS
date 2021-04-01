6568e7bd6faf92414f4ceba1ef1fef5ac256b5c1
Cristina Petrache <cristinapetrache99@gmail.com>
2021-04-01 16:54:46 +0300

    Builder - finished

--- Seminar1087/src/ro/ase/csie/cts/sem6/builder/SuperErou.java
+++ Seminar1087/src/ro/ase/csie/cts/sem6/builder/SuperErou.java
@@ -2,22 +2,22 @@ package ro.ase.csie.cts.sem6.builder;
 
 public class SuperErou {
 
-	public String nume;
-	public int puncteViata;
+	private String nume;
+	private int puncteViata;
 	
-	boolean esteNegativ;
-	boolean esteRanitGrav;
+	private boolean esteNegativ;
+	private boolean esteRanitGrav;
 	
-	InterfataArma armaStanga;
-	InterfataArma armaDreapta;
-	InterfataSuperPutere superPutere;
-	InterfataSuperPutere superSuperPutere;
+	private InterfataArma armaStanga;
+	private InterfataArma armaDreapta;
+	private InterfataSuperPutere superPutere;
+	private InterfataSuperPutere superSuperPutere;
 	
-	public SuperErou() {
+	private SuperErou() {
 		
 	}
 
-	public SuperErou(String nume, int puncteViata, boolean esteNegativ, boolean esteRanitGrav,
+	private SuperErou(String nume, int puncteViata, boolean esteNegativ, boolean esteRanitGrav,
 			InterfataArma armaStanga, InterfataArma armaDreapta, InterfataSuperPutere superPutere,
 			InterfataSuperPutere superSuperPutere) {
 		super();
@@ -31,6 +31,52 @@ public class SuperErou {
 		this.superSuperPutere = superSuperPutere;
 	}
 	
+	public static class SuperErouBuilder {
+		SuperErou superErou;
+
+		public SuperErouBuilder(String nume, int puncteViata) {
+			this.superErou = new SuperErou();
+			this.superErou.nume = nume;
+			this.superErou.puncteViata = puncteViata;
+		}
+		
+		public SuperErouBuilder esteNegativ() {
+			this.superErou.esteNegativ = true;
+			return this;
+		}
+		
+		public SuperErouBuilder esteRanit() {
+			this.superErou.esteRanitGrav = true;
+			return this;
+		}
+		
+		public SuperErouBuilder setArmaStanga(InterfataArma arma) {
+			this.superErou.armaStanga = arma;
+			return this;
+		}
+		
+		public SuperErouBuilder setArmaDreapta(InterfataArma arma) {
+			this.superErou.armaDreapta = arma;
+			return this;
+		}
+		
+		public SuperErouBuilder setSuperPutere(InterfataSuperPutere superPutere) {
+			this.superErou.superPutere = superPutere;
+			return this;
+		}
+		
+		public SuperErouBuilder setSuperSuperPutere(InterfataSuperPutere superSuperPutere) {
+			this.superErou.superSuperPutere = superSuperPutere;
+			return this;
+		}
+		
+		public SuperErou build() {
+			return this.superErou;
+		}
+		
+		
+		
+	}
 	
 	
 
