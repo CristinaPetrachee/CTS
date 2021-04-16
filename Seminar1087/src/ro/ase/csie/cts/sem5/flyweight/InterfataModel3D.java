e5ff1c415a001ff498aa3d0a02e88a1600d412c6
Cristina Petrache <cristinapetrache99@gmail.com>
2021-04-16 11:24:42 +0300

    Implementare Flyweight

---
+++ Seminar1087/src/ro/ase/csie/cts/sem5/flyweight/InterfataModel3D.java
@@ -0,0 +1,9 @@
+package ro.ase.csie.cts.sem5.flyweight;
+
+public interface InterfataModel3D {
+
+	public void incarcaModel3D(String fisier);
+	public void afisareEcran(ContextEcran context);
+	
+	
+}
