b37c285986c859142134dc85be1458b5293fe3d8
Cristina Petrache <cristinapetrache99@gmail.com>
2021-05-05 15:35:58 +0300

    Implementare Command fara manager

---
+++ Seminar1087/src/ro/ase/csie/cts/sem10/command/ModulAutoSave.java
@@ -0,0 +1,11 @@
+package ro.ase.csie.cts.sem10.command;
+
+public class ModulAutoSave implements InterfataModulJoc{
+
+	@Override
+	public void executaActiune(String numeActiune) {
+		System.out.println(numeActiune);
+		
+	}
+
+}
